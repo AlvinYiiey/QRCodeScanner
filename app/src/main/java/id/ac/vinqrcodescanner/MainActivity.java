fd801bdf4c758b1578e68e875a738b78b3453c40
Achmad Saifuloh <AchmadSaifuloh>
2022-11-12 22:08:26 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/vinqrcodescanner/MainActivity.java
@@ -0,0 +1,24 @@
+package id.ac.vinqrcodescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void scan(View view) {
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
